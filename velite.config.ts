dd774f50979836864a9b436ec20004d5c76739b6
annalhq <155549764+annalhq@users.noreply.github.com>
2025-03-15 02:27:38 +0530

    chore: setup velite and rehype plugins

--- velite.config.ts
+++ velite.config.ts
@@ -1,4 +1,48 @@
-// import { defineCollection, defineConfig, s } from "velite";
-// import rehypeAutolinkHeadings from "rehype-autolink-headings";
-// import rehypePrettyCode from "rehype-pretty-code";
-// import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import { defineCollection, defineConfig, s } from "velite";
+
+const posts = defineCollection({
+  name: "posts",
+  pattern: "content/blog/**/*.mdx",
+  schema: s.object({
+    title: s.string(),
+    description: s.string(),
+    date: s.string(),
+    tags: s.array(s.string()).optional(),
+    draft: s.boolean().optional(),
+    content: s.mdx(),
+  }),
+});
+
+const spaces = defineCollection({
+  name: "spaces",
+  pattern: "content/space/**/*.mdx",
+  schema: s.object({
+    title: s.string(),
+    description: s.string(),
+    date: s.string(),
+    tags: s.array(s.string()).optional(),
+    draft: s.boolean().optional(),
+    content: s.mdx(),
+  }),
+});
+
+export default defineConfig({
+  root: "content",
+  collections: { posts, spaces },
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
+  },
+});
