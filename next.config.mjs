2ac0d30dc9d635748c496b2811d736e8fe629855
annalhq <155549764+annalhq@users.noreply.github.com>
2025-03-14 23:29:00 +0530

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./src/env/server.ts");
+await jiti.import("./src/env/server.ts");
 
 const nextConfig = {
   /* config options here */
