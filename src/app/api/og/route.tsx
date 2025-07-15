bc39216e8822025d0e74af847c4fb14f401a5590
annalhq <155549764+annalhq@users.noreply.github.com>
2025-07-16 00:10:53 +0530

    fix(og): remove edge runtime

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -2,8 +2,6 @@ import { ImageResponse } from "next/og";
 
 import { type OgImageProps } from "@/lib/og";
 
-export const runtime = "edge";
-
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
